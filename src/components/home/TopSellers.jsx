0bb46641ff0be90736dc645eb8c2249a3aafcb02
Makram M. Ibrahim <61184060+pharaohmak@users.noreply.github.com>
2024-08-12 23:30:25 -0600

    dynamic top sellers feature

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
+import { ShimmerDiv, ShimmerText, ShimmerTitle } from "shimmer-effects-react";
 
 const TopSellers = () => {
+  const [topSellers, setTopSellers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    fetchTopSellers();
+  }, [])
+
+  async function fetchTopSellers() {
+    try {
+      const response = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers")
+      setTopSellers(response.data);
+      console.log(topSellers)
+      setLoading(false);
+    } catch (error) {
+      console.error("Counld not fetch data" + error);
+      setLoading(false)
+    }
+  }
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,24 +36,58 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
+              {loading ? (
+                new Array(12).fill(0).map((_, index) => (
+                  <li key={index}>
+                    <div className="author_list_pp">
+                      <Link to="/author">
+                        <ShimmerDiv
+                          className="lazy pp-coll"
+                          mode="light"
+                          center={true}
+                          height={50}
+                          width={50}
+                          rounded={50}
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <ShimmerTitle
+                        mode="light"
+                        center={true}
+                        width={60}
+                        line={1}
                       />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+                      <ShimmerText
+                        mode="light"
+                        center={true}
+                        width={40}
+                        line={1}
+                      />
+                    </div>
+                  </li>
+                ))
+              ) : (
+                topSellers.map((data, index) => (
+                  <li key={index}>
+                    <div className="author_list_pp">
+                      <Link to={`/author/${data.authorId}`}>
+                        <img
+                          className="lazy pp-author"
+                          src={data.authorImage}
+                          alt=""
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to={`/author/${data.authorId}`}>{data.authorName}</Link>
+                      <span>{data.price} ETH</span>
+                    </div>
+                  </li>
+                ))
+              )}
             </ol>
           </div>
         </div>
